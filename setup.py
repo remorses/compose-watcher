cc6f2225d56a726233e9e738d106d12e800aaad2
bump-version <bump@version.com>
2020-03-14 13:16:01 +0000

    1.1.2
    [skip ci]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ NAME = 'compose-watcher'
 setup(
     name=NAME,
     # [bump]
-    version='1.1.1',
+    version='1.1.2',
     description=NAME,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
